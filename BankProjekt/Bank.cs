655ebc6be1cee86c8614c7ba51173152d28dbc48
Budaházi Bence <budahazi.bence@petrik.hu>
2021-05-27 14:36:27 +0200

    Egyenleg függvény megvalósítása

--- BankProjekt/Bank.cs
+++ BankProjekt/Bank.cs
@@ -80,7 +80,8 @@ namespace BankProjekt
         // Lekérdezi az adott számlán lévő pénzösszeget
         public ulong Egyenleg(string szamlaszam)
         {
-            throw new NotImplementedException();
+            Szamla szamla = SzamlaKeres(szamlaszam);
+            return szamla.Egyenleg;
         }
     }
 
